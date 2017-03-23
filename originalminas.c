eab1530a2a715cf611889c5f5be8faad01962311
scocarojas <scocarojas@javerianacali.edu.co>
2017-03-22 23:27:37 -0500

    Update originalminas.c

--- originalminas.c
+++ originalminas.c
@@ -8,6 +8,7 @@ struct juego{  // DEFINIR LAS ESTRUCTURAS EN LAS MATRICES PARA QUE  SE PUEDAN CO
 	char cobertura;
 	char simbominas;
 	int  numminas;
+	int  nummarco;
 };
 
 typedef struct juego tipoCasilla; // para no escribir Struct casillas
@@ -15,170 +16,194 @@ typedef struct juego tipoCasilla; // para no escribir Struct casillas
 tipoCasilla** armarmatriz(int filas,int cols,tipoCasilla **matriz){   // ESTA FUNCION ME VA A organizar  LA MATRIZ
 	int i;
 	for(i=0;i<filas;i++){
-		matriz[i]=(tipoCasilla *)malloc(cols*sizeof(tipoCasilla)); // por cada fila reserva una columna de memoria
+		matriz[i]=(tipoCasilla *)malloc((cols+2)*sizeof(tipoCasilla)); // por cada fila reserva una columna de memoria
 	}
 }
 /////////////////////////////////////////////////////////////////////////777
-void rellenar(int filas, int cols,tipoCasilla **matriz) {
- 	int i,j,k,l;
+void rellenar(int filas, int cols,int nminas,tipoCasilla **matriz) {
+ 	int a,b,i,j,k,l;
 	srand(time(NULL));
-  	for (i = 0; i < filas; i++){
-    		for ( j = 0; j < cols; j++){
-      			matriz[i][j].cobertura='O';
-			k=(rand()%(filas-2))+1;
-			l=(rand()%(cols-2))+1;
+	for (size_t a = 1; a < filas-1; a++) {
+		for (size_t b = 1; b < cols-1; b++) {
+			matriz[a][b].cobertura='*';
+			/* code */
+		}
+		/* code */
+	}
+	for (size_t x = 0; x < filas; x++) {
+			matriz[0][x].nummarco=x;
+			matriz[0][x].numminas=1;
+			if (x==filas-1) {
+				for (size_t y = 0; y < cols; y++) {
+					matriz[y][0].nummarco=y;
+					matriz[y][0].numminas=1;
+				}
+				/* code */
+	}
+}
+
+	for (size_t i = 0; i <nminas; i++) {
+		k=1+(rand()%(filas-2));
+		l=1+(rand()%(cols-2));
+		printf("%d%d\n",k,l );
+		if (matriz[k][l].simbominas=='M') {
+			k=1+(rand()%(filas)-2);
+			l=1+(rand()%(cols)-2);
+			printf("%d%d\n",k,l );
 			matriz[k][l].simbominas='M';
-    		}
-  	}
+		}
+		else{
+		matriz[k][l].simbominas='M';
+		}
+	}
 }
 ///////////////////////////////////////////////////////////////////////////////7
 void imprimir(int filas, int cols, tipoCasilla** matriz){
 
 	int i;
-  	int j;
-	for(i = 0; i < filas; i++){
+  int j;
+	for(i = 0; i < filas-1; i++){
 		printf("\n");
 		printf("|");
-		for (j = 0; j < cols; ++j){
-      			
-			printf(" %c\t ",matriz[i][j].cobertura);
+		for (j = 0; j < cols-1; ++j){
+			printf(" %c\t",matriz[i][j].cobertura/*,matriz[i][j].numminas,matriz[i][j].simbominas,matriz[i][j].nummarco*/);
+		//	printf(" %c%d\t",matriz[i][j].cobertura,matriz[i][j].nummarco);
+		//	printf(" %c\t",matriz[i][j].cobertura);
 		}
 		printf("|");
-   		printf("\n");
+   	printf("\n");
 	}
 	printf("\n");
 }
 
 void numerodeminas(int filas,int cols, tipoCasilla** matriz){
 
-	int i,j;
-	for(i=1;i<filas-1;i++){
-		for(j=1;j<cols-1;++j){
-			if(matriz[i][j].simbominas!='M'){
-				if(matriz[i-1][j-1].simbominas=='M'){
+	int i,j,contMin;
+	for (i=1; i < filas-1; i ++){
+		for(j=1; j<cols-1; j++){
+			if (matriz[i][j].simbominas == 'M'){
+				matriz[i][j].numminas=1;
+				if(matriz[i-1][j-1].simbominas != 'M'){
+					matriz[i-1][j-1].numminas+=1;
 
-					matriz[i][j].numminas+=1;
-				}
+				}if(matriz[i][j-1].simbominas !='M'){
+					matriz[i][j-1].numminas+=1;
 
-				if(matriz[i-1][j].simbominas=='M'){
+				}if (matriz[i-1][j].simbominas != 'M'){
+					matriz[i-1][j].numminas+=1;
 
-					matriz[i][j].numminas+=1;
-				}
+				}if (matriz[i-1][j+1].simbominas != 'M'){
+					matriz[i-1][j+1].numminas +=1;
 
-				if(matriz[i][j-1].simbominas=='M'){
+				}if (matriz[i][j+1].simbominas != 'M'){
+					matriz[i][j+1].numminas+=1;
 
-					matriz[i][j].numminas+=1;
-				}
+				}if (matriz[i+1][j-1].simbominas != 'M'){
+					matriz[i+1][j-1].numminas+=1;
 
-				if(matriz[i+1][j+1].simbominas=='M'){
+				}if (matriz[i+1][j].simbominas != 'M'){
+					matriz[i+1][j].numminas+=1;
 
-					matriz[i][j].numminas+=1;
-				}
-
-				if(matriz[i][j+1].simbominas=='M'){
-
-					matriz[i][j].numminas+=1;
-				}	
-				
-				if(matriz[i+1][j].simbominas=='M'){
-
-					matriz[i][j].numminas+=1;
-				}
-
-				if(matriz[i+1][j-1].simbominas=='M'){
-
-					matriz[i][j].numminas+=1;
-				}
-
-				if(matriz[i-1][j+1].simbominas=='M'){
-
-					matriz[i][j].numminas+=1;
+				}if (matriz[i+1][j+1].simbominas != 'M'){
+					matriz[i+1][j+1].numminas+=1;
 				}
 			}
 		}
 	}
 }
 
+
 void destapar(int filas, int cols, tipoCasilla** matriz){
 
-	int posFilas,posCols,i,j;
+	int posFilas,posCols,i,j,respuesta;
+
 
 	while(1){
-	
+
 		printf("Introduzca la posición:\n");
-    		printf("Fila:\n");
-    		scanf("%d", &posFilas);
-    		printf("Columna:\n");
-    		scanf("%d", &posCols);
-    		printf("\n");
+    	printf("Fila:\n");
+    	scanf("%d", &posFilas);
+    	printf("Columna:\n");
+    	scanf("%d", &posCols);
+    	printf("\n");
 
-    		if(matriz[posFilas][posCols].simbominas=='M'){
-
-    			imprimir(filas,cols,matriz);
-    			printf("Destapaste una mina. Has perdido\n");
-    			break;
-    		}
-    		else{
+	    	if(matriz[posFilas][posCols].simbominas=='M'){
+			matriz[posFilas][posCols].cobertura='X';
 			imprimir(filas,cols,matriz);
-			for(i=1;i<filas-1;i++){
-				for(j=1;j<cols-1;++j){
-					if(matriz[i-1][j-1].simbominas=='M'){
+			printf("Destapaste una mina. Has perdido\n");
+			printf("Desea Volver a Jugar?, 1 es si , 0 es no \n");
+			printf("repuesta:\n");
+			break;
+		}
+		else{
+			matriz[posFilas][posCols].cobertura=matriz[posFilas][posCols].numminas+'0';
+			imprimir(filas,cols,matriz);
 
-						matriz[i][j].cobertura=(char)(matriz[i][j].numminas);
+			if((posFilas>0)&&(posCols>0)){
+
+				if(matriz[posFilas][posCols].numminas==0){
+					printf("entró\n");
+					matriz[posFilas][posCols].cobertura='0';
+
+					if(matriz[posFilas-1][posCols-1].numminas==0){
+
+						matriz[posFilas-1][posCols-1].cobertura='0';
+
 					}
 
-					if(matriz[i-1][j].simbominas=='M'){
-				
-						matriz[i][j].cobertura=(char)(matriz[i][j].numminas);
+					if(matriz[posFilas-1][posCols].numminas==0){
+
+						matriz[posFilas-1][posCols].cobertura='0';
 					}
 
-					if(matriz[i][j-1].simbominas=='M'){
+					if(matriz[posFilas][posCols-1].numminas==0){
 
-						matriz[i][j].cobertura=(char)(matriz[i][j].numminas);
+						matriz[posFilas][posCols-1].cobertura='0';
 					}
 
-					if(matriz[i+1][j+1].simbominas=='M'){
+					if(matriz[posFilas+1][posCols+1].numminas==0){
 
-						matriz[i][j].cobertura=(char)(matriz[i][j].numminas);
+						matriz[posFilas+1][posCols+1].cobertura='0';
 					}
 
-					if(matriz[i][j+1].simbominas=='M'){
+					if(matriz[posFilas][posCols+1].numminas==0){
 
-						matriz[i][j].cobertura=(char)(matriz[i][j].numminas);
-					}	
-				
-					if(matriz[i+1][j].simbominas=='M'){
-
-						matriz[i][j].cobertura=(char)(matriz[i][j].numminas);
+						matriz[posFilas][posCols+1].cobertura='0';
 					}
 
-					if(matriz[i+1][j-1].simbominas=='M'){
+					if(matriz[posFilas+1][posCols].numminas==0){
 
-						matriz[i][j].cobertura=(char)(matriz[i][j].numminas);
+						matriz[posFilas+1][posCols].cobertura='0';
 					}
 
-					if(matriz[i-1][j+1].simbominas=='M'){
+					if(matriz[posFilas+1][posCols-1].numminas==0){
 
-						matriz[i][j].cobertura=(char)(matriz[i][j].numminas);
+						matriz[posFilas+1][posCols-1].cobertura='0';
 					}
-		
+
+					if(matriz[posFilas-1][posCols+1].numminas==0){
+
+						matriz[posFilas-1][posCols+1].cobertura='0';
+					}				
 				}
 			}
-    		}
+		}
 	}
+
 }
 
 ////////////////////////////////////////////7
 void main(int argc, char *argv[]){
 
-	int filas=atoi(argv[1]);
-	int cols=atoi(argv[2]);
-  	tipoCasilla **matriz;
-  	matriz=(tipoCasilla**)malloc(filas*sizeof(tipoCasilla*)); // reserva de memoria para las filas de la matriz
+	int filas=(atoi(argv[1]))+2;
+	int cols=(atoi(argv[2]))+2;
+	int nminas=atoi(argv[3]);
+ 	tipoCasilla **matriz;
+ 	matriz=(tipoCasilla**)malloc((filas+2)*sizeof(tipoCasilla*)); // reserva de memoria para las filas de la matriz
 	armarmatriz(filas,cols,matriz);
-	rellenar(filas,cols,matriz);
-	imprimir(filas,cols,matriz);
+	rellenar(filas,cols,nminas,matriz);
 	numerodeminas(filas,cols,matriz);
+	imprimir(filas,cols,matriz);
 	destapar(filas,cols,matriz);
 }
 ///# Miniproyecto
